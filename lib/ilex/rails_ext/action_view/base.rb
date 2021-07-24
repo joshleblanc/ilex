e3eda728fd30bcb15ebb3f5234229b644bbcf59a
Joshua LeBlanc <joshleblanc94@gmail.com>
2021-07-24 16:11:38 -0300

    fix: when block returns an arbre element, render context

--- lib/ilex/rails_ext/action_view/base.rb
+++ lib/ilex/rails_ext/action_view/base.rb
@@ -5,11 +5,18 @@ module Ilex
         def capture(*args)
           value = nil
           buffer = with_output_buffer { value = yield(*args) }
-
-          # Override to handle Arbre elements inside helper blocks.
-          # See https://github.com/rails/rails/issues/17661
-          # and https://github.com/rails/rails/pull/18024#commitcomment-8975180
-          value = value.to_s if value.is_a?(Arbre::Element)
+        
+          # if args.first&.respond_to? :ctx 
+          #   value = args.first.ctx.children.to_s
+          # end
+          # We're only capturing the last returned arbre element
+          # This uses that element to get the current context, and 
+          # get the html for the whole tree
+          # We have to collect the children manually because 
+          # `content` is overridden 
+          if value.is_a? Arbre::Element
+            value = value.arbre_context.children.to_s
+          end
 
           if (string = buffer.presence || value) && string.is_a?(String)
             ERB::Util.html_escape string
